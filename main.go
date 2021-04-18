0a8c5614b153b928f90941d79fc5efe75d63e6f7
DmitryKrom <krom77@hotmail.de>
2021-04-18 21:25:01 +0200

    hotfix: Map of Notes added

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var nums []int = []int{1, 1, 5, 5, 3, 4, 3, 1, 1, 5, 5, 5, 5, 3, 3, 4, 3, 4, 4, 1}
-	highestRank(nums)
+	var listOfNotes []int = []int{1, 1, 5, 5, 3, 4, 3, 1, 1, 5, 5, 5, 5, 3, 3, 4, 3, 4, 4, 1}
+	highestRank(listOfNotes)
 }
 
-func highestRank(nums []int) map[int]int {
+func highestRank(listOfNotes []int) map[int]int {
 	var notes = map[int]int{1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
 
 	for key := range notes {
 		var b int = 0
-		for j := 0; j < len(nums); j++ {
-			if key == nums[j] {
+		for j := 0; j < len(listOfNotes); j++ {
+			if key == listOfNotes[j] {
 				b++
 			}
 		}
